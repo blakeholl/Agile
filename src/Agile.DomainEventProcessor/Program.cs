f5cec59ddf8a678180254c3b8056174b9d9b0e80
Blake Hollingsworth <blake.hollingsworth@gmail.com>
2014-11-14 22:37:05 -0500

    Playing around with reading events/metadata

--- src/Agile.DomainEventProcessor/Program.cs
+++ src/Agile.DomainEventProcessor/Program.cs
@@ -6,17 +6,22 @@ using System.Text;
 using System.Threading.Tasks;
 using EventStore.ClientAPI;
 using EventStore.ClientAPI.SystemData;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Ninject;
 
 namespace Agile.DomainEventProcessor
 {
     class Program
     {
+        private const string AggregateClrTypeHeader = "AggregateClrTypeName";
+        private const string EventClrTypeHeader = "EventClrTypeName";
+
         static void Main(string[] args)
         {
             var kernel = ConfigureKernel();
             var connection = kernel.Get<IEventStoreConnection>();
-            Task.Factory.StartNew(() => ConfigureSub(connection));
+            ConfigureSub(connection);
             Console.ReadLine();
         }
 
@@ -36,22 +41,51 @@ namespace Agile.DomainEventProcessor
             return kernel;
         }
 
-        static async Task ConfigureSub(IEventStoreConnection eventStoreConnection)
+        static void ConfigureSub(IEventStoreConnection eventStoreConnection)
         {
-            var sub =
-                await
-                    eventStoreConnection.SubscribeToAllAsync(false, EventAppeared, SubscriptionDropped,
-                        new UserCredentials("admin", "changeit"));
+            eventStoreConnection.SubscribeToAllFrom(Position.Start, false, EventAppeared, LiveProcessingStarted,
+                SubscriptionDropped, new UserCredentials("admin", "changeit"));
         }
 
-        private static void SubscriptionDropped(EventStoreSubscription eventStoreSubscription, SubscriptionDropReason subscriptionDropReason, Exception arg3)
+        private static void SubscriptionDropped(EventStoreCatchUpSubscription eventStoreCatchUpSubscription, SubscriptionDropReason subscriptionDropReason, Exception arg3)
         {
-            throw new NotImplementedException();
+            
         }
 
-        private static void EventAppeared(EventStoreSubscription eventStoreSubscription, ResolvedEvent resolvedEvent)
+        private static void LiveProcessingStarted(EventStoreCatchUpSubscription eventStoreCatchUpSubscription)
         {
-            Console.WriteLine(resolvedEvent.Event.Data);
+            
+        }
+
+        private static void EventAppeared(EventStoreCatchUpSubscription eventStoreCatchUpSubscription, ResolvedEvent resolvedEvent)
+        {
+            if (IsSystemEvent(resolvedEvent))
+            {
+                return;
+            }
+
+            Console.WriteLine(resolvedEvent.OriginalEvent.EventStreamId);
+            Console.WriteLine(Guid.Parse(resolvedEvent.OriginalStreamId.Split('-').Last()).ToString("B"));
+            Console.WriteLine(resolvedEvent.OriginalEventNumber);
+            var @event = DeserializeEvent(resolvedEvent.Event.Metadata, resolvedEvent.Event.Data);
+            Console.WriteLine(@event.GetType().FullName);
+        }
+
+        private static dynamic DeserializeEvent(byte[] metadata, byte[] data)
+        {
+            var meta = JObject.Parse(Encoding.UTF8.GetString(metadata));
+
+            var aggregateClrTypeName = meta.Property(AggregateClrTypeHeader).Value;
+            var eventClrTypeName = meta.Property(EventClrTypeHeader).Value;
+
+            Console.WriteLine("{0} {1}", aggregateClrTypeName, eventClrTypeName);
+            
+            return JsonConvert.DeserializeObject(Encoding.UTF8.GetString(data), Type.GetType((string)eventClrTypeName));
+        }
+
+        private static bool IsSystemEvent(ResolvedEvent resolvedEvent)
+        {
+            return resolvedEvent.Event.EventType.StartsWith("$");
         }
     }
 }
